48a7f7c93bce226ebc73516e1110d6dfe524cf16
Daniel <daniel19brizuela@gmail.com>
2022-02-02 14:20:41 +0100

    validaciones y empezando con informe

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'report'],
 
     # always loaded
     'data': [
@@ -28,7 +28,7 @@
         'views/views.xml',
         'views/templates.xml',
         'views/pieza.xml',
-        'views/cliente.xml',
+        'reportPieza.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- models/Pieza.py
+++ models/Pieza.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from odoo import api
+from odoo import exceptions
 from odoo import fields
 from odoo import models
-from odoo import exceptions
 
 class Pieza(models.Model):
     _name = 'gesre.pieza'
@@ -18,7 +18,26 @@ class Pieza(models.Model):
     trabajador = fields.Many2one('gesre.trabajador', string="Trabajador")
     
     #Validaciones
+    #Numero negativo y longitud en Stock
     @api.constrains('stock')
     def _verificar_stock_no_negativo(self):
         if self.stock < 0:
             raise exceptions.ValidationError("Stock no puede ser negativo")
+        if len(str(self.stock)) > 3:
+            raise exceptions.ValidationError("Solo se admiten numeros de 3 dígitos en Stock")
+    
+    #Longitud en Nombre
+    @api.onchange('nombre')
+    def _limitar_longitud_campo_nombre(self):
+        if len(self.nombre) > 25:
+            return {
+                'warning': {
+                    'title': "Límite alcanzado",
+                    'message': "Límite de 25 caracteres alcanzado en Nombre",
+                },
+            }
+            
+    @api.constrains('nombre')
+    def _limitar_longitud_campo_nombre(self):
+        if len(self.nombre) > 25:
+            raise exceptions.ValidationError("Límite de 25 caracteres alcanzado en Nombre")
