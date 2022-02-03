309b6c83f28a4782c536296484dd7a84cea5993a
mikelmatilla <mikel.matilla1@gmail.com>
2022-02-03 11:47:45 +0100

    Restricciones hechas y report empezado

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'report'],
 
     # always loaded
     'data': [
@@ -28,6 +28,8 @@
         'views/views.xml',
         'views/templates.xml',
         'views/pieza.xml',
+        'views/trabajador.xml',
+        'reportTrabajador.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
